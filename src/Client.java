22728da26eb8d0018f1ab0f0b024aef59f774a9b
KSanders04 <kasanders2004@gmail.com>
2025-02-14 10:27:12 -0500

    Add legacy code to client and server. Add menu to client side for user input

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,52 @@
+import java.io.FileOutputStream;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Please provide <serverIP> and <serverPort>");
+            return;
+        }
+        int serverPort = Integer.parseInt(args[1]);
+        Scanner keyboard = new Scanner(System.in);
+        String filename = keyboard.nextLine();
+        SocketChannel channel = SocketChannel.open();
+        channel.connect(
+                new InetSocketAddress(args[0], serverPort)
+        );
 
+        while (true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please choose an option:\n" +
+                    "1. List files\n" +
+                    "2. Delete a file\n" +
+                    "3. Rename a file\n" +
+                    "4. Download a file from server\n" +
+                    "5. Upload a file to server\n" +
+                    ">> ");
+            switch (){
+
+            }
+        }
+        ByteBuffer buffer = ByteBuffer.wrap(filename.getBytes());
+        channel.write(buffer);
+        channel.shutdownOutput();
+
+        FileOutputStream fs = new FileOutputStream(
+                "ClientFiles/"+filename, true
+        );
+        FileChannel fc = fs.getChannel();
+        ByteBuffer fileContent =
+                ByteBuffer.allocate(1024);
+        while(channel.read(fileContent) >=0) {
+            fileContent.flip();
+            fc.write(fileContent);
+            fileContent.clear();
+        }
+        fs.close();
+        channel.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,45 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 public class Server {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel listenChannel =
+                ServerSocketChannel.open();
+        listenChannel.bind(new InetSocketAddress(3000));
+        while(true){
+            SocketChannel serveChannel =
+                    listenChannel.accept();
+            ByteBuffer buffer =
+                    ByteBuffer.allocate(1024);
+            int bytesRead = serveChannel.read(buffer);
+            buffer.flip();
+            byte[] a = new byte[bytesRead];
+            buffer.get(a);
+            String fileName = new String(a);
+            System.out.println("File name: "+fileName);
+            File file = new File("ServerFiles/"+fileName);
+            if (!file.exists()){
+                System.out.println("File doesn't exist");
+            }else{
+                FileInputStream fs =
+                        new FileInputStream(file);
+                FileChannel fc = fs.getChannel();
+                ByteBuffer fileContent =
+                        ByteBuffer.allocate(1024);
+                int byteRead = 0;
+                do {
+                    byteRead = fc.read(fileContent);
+                    fileContent.flip();
+                    serveChannel.write(fileContent);
+                    fileContent.clear();
+                }while(byteRead>=0);
+                fs.close();
+            }
+            serveChannel.close();
+        }
     }
 }
